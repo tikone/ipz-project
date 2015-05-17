1dfbd0370927af560afbf096e9884f1e4ed987a1
tikone <tikhon.tik@gmail.com>
2015-05-17 23:13:43 +0300

    add ExcursionTest.cs

--- TravelAgency/TravelAgencyModel/Guide.cs
+++ TravelAgency/TravelAgencyModel/Guide.cs
@@ -5,18 +5,62 @@ using System.Text;
 
 namespace TravelAgencyModel
 {
-    class Guide
+    public class Guide
     {
 
         #region public fields
 
-            public String Name { get; set; }
+            public String Name { get; private set; }
 
-            public String Language { get; set; }
+            public HashSet< String > Languages { get; private set; }
 
             public Boolean Available { get; set; }
 
-            public Int32 Phone { get; set; }
+            public Int32 Phone { get; private set; }
+
+        #endregion
+
+        #region constructors
+
+            public Guide(
+                    String _name
+                ,   String _lanuage
+                ,   Int32 _phone
+            )
+            {
+                this.Name = _name;
+                this.Phone = _phone;
+
+                this.Available = true;
+
+                this.Languages = new HashSet<string>();
+                Languages.Add(_lanuage);
+            }
+
+            public Guide(
+                    String _name
+                ,   HashSet<String> _lanuage
+                ,   Int32 _phone
+            )
+            {
+                this.Name = _name;
+                this.Phone = _phone;
+
+                this.Languages = _lanuage;
+            }
+
+        #endregion
+
+        #region public methods
+
+            public void AddLanguage( String _language )
+            {
+                if( Languages.Contains( _language ) )
+                    throw new Exception( @"not unique language of Guide." );
+
+                Languages.Add( _language );
+
+            }
 
         #endregion
 
