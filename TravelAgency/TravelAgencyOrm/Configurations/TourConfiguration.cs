55e2a5560c675978e3396bc8f0e6e8e3466b24a3
Kirill Oleynik <Snoop5176@yandex.ru>
2015-05-31 12:33:53 -0700

    init new projects for lab5

---
+++ TravelAgency/TravelAgencyOrm/Configurations/TourConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using TravelAgencyModel;
+
+using System.Data.Entity.ModelConfiguration;
+
+namespace TravelAgencyOrm.Configurations
+{
+    class TourConfiguration
+        :   EntityTypeConfiguration< Tour >
+    {
+        public TourConfiguration()
+        {
+
+        }
+    }
+}
---
+++ TravelAgency/TravelAgencyOrm/TravelAgencyDbContext.cs
@@ -0,0 +1,26 @@
+﻿using TravelAgencyModel;
+
+using System.Data.Entity;
+
+namespace TravelAgencyOrm
+{
+    class TravelAgencyDbContext
+        :   DbContext
+    {
+        static TravelAgencyDbContext()
+        {
+            Database.SetInitializer(
+                new DropCreateDatabaseIfModelChanges< TravelAgencyDbContext >()
+            );
+        }
+
+        //TODO: add properties gateways DbSet
+
+
+        protected override void OnModelCreating( DbModelBuilder modelBuilder )
+        {
+            // TODO: build model with configuration
+            // modelBuilder.Configurations.Add( new PizzaRecipeConfiguration() );
+        }
+    }
+}
