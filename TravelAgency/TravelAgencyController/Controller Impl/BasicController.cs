e45513d05bf8846a6c9f8cafbe2b19517259520c
tikone <tikhon.tik@gmail.com>
2015-10-18 18:18:26 +0300

    BasicController.cs

---
+++ TravelAgency/TravelAgencyController/Controller Impl/BasicController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using TravelAgencyOrm;
+
+namespace TravelAgencyController.Controller
+{
+    class BasicController
+    {
+
+        protected TravelAgencyDbContext GetDBContext ()
+        {
+            if( m_dbContext == null )
+                m_dbContext = new TravelAgencyDbContext();
+
+            return m_dbContext;
+
+        }
+
+        public void Dispose ()
+        {
+            Dispose( true );
+            GC.SuppressFinalize( this );
+        }
+
+        protected virtual void Dispose ( bool _disposing )
+        {
+            if ( _disposing && m_dbContext != null )
+                m_dbContext.Dispose();
+        }
+
+        private TravelAgencyDbContext m_dbContext;
+
+    }
+}
