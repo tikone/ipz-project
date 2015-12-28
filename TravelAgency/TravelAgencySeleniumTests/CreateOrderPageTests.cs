609d3a86f7c811b090b104fe454c08a4483e4bdc
Kirill Oleynik <Snoop5176@yandex.ru>
2015-12-29 01:35:45 +0200

    selenium test

---
+++ TravelAgency/TravelAgencySeleniumTests/CreateOrderPageTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+using NUnit.Framework;
+
+namespace TravelAgencySeleniumTests
+{
+    public class CreateOrderPageTests
+    {
+        private IWebDriver driver = new ChromeDriver();
+
+        [SetUp]
+        public void initialize()
+        {
+            driver.Navigate().GoToUrl("http://localhost:63784/ManageTour/Index");
+        }
+
+        [Test]
+        public void inputValues()
+        {
+            SeleniumSetMethods.enterText( driver, "Country", "USA", "Id" );
+
+            SeleniumSetMethods.selectDropDown( driver, "Type", "Beer", "Id" );
+
+            SeleniumSetMethods.enterText( driver, "Description", "some description", "Name" );
+
+            SeleniumSetMethods.click( driver, "create_tour", "Name" );
+        }
+
+        [TearDown]
+        public void cleanUp()
+        {
+            driver.Close();
+        }
+    }
+}
---
+++ TravelAgency/TravelAgencySeleniumTests/SeleniumSetMethods.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace TravelAgencySeleniumTests
+{
+    class SeleniumSetMethods
+    {
+        public static void enterText(
+                IWebDriver _driver
+            ,   string _element
+            ,   string _value
+            ,   string _elementType
+        )
+        {
+            if( _elementType == "Id" )
+                _driver.FindElement( By.Id( _element ) ).SendKeys( _value );
+            if ( _elementType == "Name" )
+                _driver.FindElement( By.Name( _element ) ).SendKeys( _value );
+        }
+
+        public static void click(
+                IWebDriver _driver
+            ,   string _element
+            ,   string _elementType
+        )
+        {
+            if( _elementType == "Id" )
+                _driver.FindElement( By.Id( _element ) ).Click();
+            if ( _elementType == "Name" )
+                _driver.FindElement( By.Name( _element ) ).Click();
+        }
+
+        public static void selectDropDown(
+                IWebDriver _driver
+            ,   string _element
+            ,   string _value
+            ,   string _elementType
+        )
+        {
+            if( _elementType == "Id" )
+                new SelectElement( _driver.FindElement( By.Id( _element ) ) ).SelectByText( _value );
+            if ( _elementType == "Name" )
+                new SelectElement( _driver.FindElement( By.Name( _element ) ) ).SelectByText(_value);
+        }
+
+    }
+}
